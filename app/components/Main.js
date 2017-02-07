6caeffd5189a25f6a6bc85d44254e5318b7c67ee
Coco Duan <coco@Cocos-MacBook-Pro.local>
2017-02-06 18:13:00 -0800

    Routing without mixins

--- app/components/Main.js
+++ app/components/Main.js
@@ -8,7 +8,7 @@ export default class Main extends React.Component {
             <div className="main-container">
                 <nav className="navbar navbar-default" role="navigation">
                     <div className="col-sm-7 col-sm-offset-2" style={{marginTop: 15}}>
-                        <SearchGithub/>
+                        <SearchGithub history={this.props.history}/>
                     </div>
                 </nav>
                 <div className="container">
--- app/components/SearchGithub.js
+++ app/components/SearchGithub.js
@@ -3,25 +3,26 @@ import {History} from 'react-router';
 
 // history.pushState
 // profile/:username -> /profile/:username
-const SearchGithub = React.createClass({
-    mixins: [History],
 
-    getRef: function(ref) {
+// let route handles history and since SearchGithub lives under Main, so we can just pass this.props.history
+
+export default class SearchGithub extends React.Component {
+    getRef(ref) {
         this.usernameRef = ref;
-    },
+    }
 
-    handleSubmit: function() {
-        let username = this.usernameRef.value;
+    handleSubmit() {
+        const username = this.usernameRef.value;
         this.usernameRef.value = '';
-        this.history.pushState(null, `/profile/${username}`);
-    },
+        this.props.history.pushState(null, `/profile/${username}`);
+    }
 
     render() {
         return (
             <div className="col-md-12">
-                <form onSubmit={this.handleSubmit}>
+                <form onSubmit={() => {this.handleSubmit()}}>
                     <div className="form-group col-md-7">
-                        <input type="text" className="form-control" ref={this.getRef} />
+                        <input type="text" className="form-control" ref={(ref) => {this.getRef(ref)}} />
                     </div>
                     <div className="form-group col-md-5">
                         <button type="submit" className="btn btn-block btn-primary">Search Github</button>
@@ -30,6 +31,8 @@ const SearchGithub = React.createClass({
             </div>
         );
     }
-});
+}
 
-export default SearchGithub;
+SearchGithub.propTypes = {
+    history: React.PropTypes.object.isRequired
+};
